6c535ababfc35a292771a24b8d3589eecbd264da
Cosette Tshibanda <cosettetshibanda@gmail.com>
2023-03-03 21:50:20 -0700

    commit tree 034a6e2dc6bed4bdb48cd34d628ad4e8fd265690

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+  const [inCart, setCart] = useState(false)
+
+  function handleCartAdding(){
+    setCart((inCart) => !inCart)
+  }
   return (
-    <li className="">
+    <li className= {inCart ? "in-cart" : "" }>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={inCart ? "remove" : "add"} onClick={handleCartAdding}> {inCart ? "Remove from" : "Add to"} Cart</button>
     </li>
   );
 }
